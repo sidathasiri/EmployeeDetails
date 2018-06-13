a69d053a89b973570da51cc48f2c05ef3d2f2c00
Sidath Asiri <sidathasiri@gmail.com>
2018-06-13 16:51:56 +0530

    added backend skelton

---
+++ EmployeeDetails/EmployeeDao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{
+    class EmployeeDao {
+        public void insertEmployee(Employee employee)
+        {
+            //insert employee
+        }
+
+        public void updateEmployee(Employee employee)
+        {
+            // update emp
+        }
+
+        public void fetchEmployee(Employee employee)
+        {
+            //fetch emp
+        }
+
+        public void deleteEmployee(Employee employee)
+        {
+            //delete emp
+        }
+    }
+}
---
+++ EmployeeDetails/EmployeeServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{
+    class EmployeeServices{
+        public void insertEmployee(Employee employee) { 
+            //insert employee
+        }
+
+        public void updateEmployee(Employee employee) { 
+            // update emp
+        }
+
+        public void fetchEmployee(Employee employee) { 
+            //fetch emp
+        }
+
+        public void deleteEmployee(Employee employee) { 
+            //delete emp
+        }
+    }
+}
